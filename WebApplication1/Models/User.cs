ce243b4f43c992ff1dbb2dc32c3fd8e757c12634
Hanna Tekie <hantek90@gmail.com>
2023-01-13 16:20:22 +0100

    added set

--- WebApplication1/Models/User.cs
+++ WebApplication1/Models/User.cs
@@ -2,14 +2,14 @@
 {
     public class User
     {
-        public int Id { get;}
-        public string Name { get;}
-        public string UserName { get;}
-        public string Password { get;  }
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string UserName { get; set; }
+        public string Password { get; set; }
 
-        public IList<Attraction> Attractions { get;}
-        public IList<Rating> Ratings { get;}
-        public IList<Comment> Comments { get;}
+        public IList<Attraction> Attractions { get; set; }
+        public IList<Rating> Ratings { get; set; }
+        public IList<Comment> Comments { get; set; }
         
 
     }
