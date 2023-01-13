57128daa84d08474f3cc258cd00699bfaa30af1c
Hanna Tekie <hantek90@gmail.com>
2023-01-13 15:55:11 +0100

    created a new project

---
+++ WebApplication1/Models/User.cs
@@ -0,0 +1,16 @@
+﻿namespace Provinceadvisor.Models
+{
+    public class User
+    {
+        public int Id { get;}
+        public string Name { get;}
+        public string UserName { get;}
+        public string Password { get;  }
+
+        public IList<Attraction> Attractions { get;}
+        public IList<Rating> Ratings { get;}
+        public IList<Comment> Comments { get;}
+        
+
+    }
+}
