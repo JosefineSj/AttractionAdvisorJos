6d7476c957653be972d7c4e6e1de52225a51c6d6
Hanna Tekie <hantek90@gmail.com>
2023-01-13 17:00:24 +0100

    added the rest of properties in the models

--- WebApplication1/Models/Comment.cs
+++ WebApplication1/Models/Comment.cs
@@ -2,5 +2,11 @@
 {
     public class Comment
     {
+        public int Id { get; set; }
+        public int AttractionId { get; set; }
+        public Attraction Attraction { get; set; }
+        public int UserId { get; set; }
+        public User User { get; set; }
+        public string Commentary { get; set; }
     }
 }
--- WebApplication1/Models/Rating.cs
+++ WebApplication1/Models/Rating.cs
@@ -2,5 +2,11 @@
 {
     public class Rating
     {
+        public int Id { get; set; }
+        public int AttractionId { get; set; }
+        public Attraction Attraction { get; set; }
+        public int UserId { get; set; }
+        public User User { get; set; }
+        public byte Rank { get; set; }
     }
 }
