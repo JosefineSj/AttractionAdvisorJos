3f9335c87358d4ada6f558d7c1b6828bebfd1cfe
Hanna Tekie <hantek90@gmail.com>
2023-01-16 12:59:53 +0100

    migrated database

---
+++ restAPI/AttractionAdvisor/AttractionAdvisor/Models/Rating.cs
@@ -0,0 +1,14 @@
+﻿namespace AttractionAdvisor.Models
+{
+
+    public class Rating
+    {
+        public int Id { get; set; }
+        public int AttractionId { get; set; }
+        public Attraction Attraction { get; set; }
+        public int UserId { get; set; }
+        public User User { get; set; }
+        public byte Rank { get; set; }
+    }
+
+}
--- restAPI/AttractionAdvisor/AttractionAdvisor/Program.cs
+++ restAPI/AttractionAdvisor/AttractionAdvisor/Program.cs
@@ -1,3 +1,15 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-Console.WriteLine("Hello, World!");
+using AttractionAdvisor.data_access;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+
+builder.Services.AddDbContext<AttractionAdvisorDbContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("AttractionAdvisorConnection")));
+
+var app = builder.Build();
+
+app.UseHttpsRedirection();
+
+app.Run();
