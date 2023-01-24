84c557f46934020e54a37b089570c27d1a76c91a
Salam4nder <kamran.personal@icloud.com>
2023-01-24 12:14:19 +0100

    update

--- restAPI/AttractionAdvisor/AttractionAdvisor/Interfaces/IUserRepository.cs
+++ restAPI/AttractionAdvisor/AttractionAdvisor/Interfaces/IUserRepository.cs
@@ -5,10 +5,10 @@ namespace AttractionAdvisor.Interfaces
     public interface IUserRepository
     {
         Task<IEnumerable<User>> GetUsers(); 
-        Task<User> GetUserById(int id); 
+        Task<User?> GetUserById(int id); 
         Task<User> AddUser(User user);
-        Task<User> UpdateUser(User user);
+        Task<User?> UpdateUser(User user);
         Task<bool> DeleteUser(int id);
-        Task<User> LoginUser(string userName, string password);
+        Task<User?> LoginUser(string userName, string password);
     }
 }
