8e17ee0d0c0f44b4ec42436136b0d5778065b466
Salam4nder <kamran.personal@icloud.com>
2023-01-30 19:01:52 +0100

    update

--- restAPI/AttractionAdvisor/AttractionAdvisor/Interfaces/IAttractionRepository.cs
+++ restAPI/AttractionAdvisor/AttractionAdvisor/Interfaces/IAttractionRepository.cs
@@ -4,6 +4,8 @@ namespace AttractionAdvisor.Interfaces
 {
     public interface IAttractionRepository
     {
+        Task<List<AttractionDto>> GetAggregatedAttractions();
+        Task<List<AttractionDto>> GetAggregatedAttractionsByUserId(int userId);
         Task<IEnumerable<Attraction>> GetAttractions();
         Task<Attraction?> GetAttraction(int id);
         Task<Attraction> AddAttraction(Attraction attraction);
