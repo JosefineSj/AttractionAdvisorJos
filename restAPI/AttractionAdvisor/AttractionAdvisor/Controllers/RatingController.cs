4704885c9b639bf8e229cdbac15a8dde541c335a
Salam4nder <kamran.personal@icloud.com>
2023-02-02 17:14:17 +0100

    update

--- restAPI/AttractionAdvisor/AttractionAdvisor/Controllers/RatingController.cs
+++ restAPI/AttractionAdvisor/AttractionAdvisor/Controllers/RatingController.cs
@@ -1,117 +1,113 @@
 ﻿using AttractionAdvisor.Interfaces;
 using AttractionAdvisor.Models;
-using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
-namespace AttractionAdvisor.Controllers
+namespace AttractionAdvisor.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class RatingController : ControllerBase
 {
-    [Route("api/[controller]")]
-    [ApiController]
-    public class RatingController : ControllerBase
+    private readonly IRatingRepository _ratingRepository;
+
+    public RatingController(IRatingRepository ratingRepository)
     {
-        private readonly IRatingRepository _ratingRepository;
+        _ratingRepository = ratingRepository;
+    }
 
-        public RatingController(IRatingRepository ratingRepository)
+    [HttpGet]
+    public async Task<ActionResult<IEnumerable<Rating>>> GetRatings()
+    {
+        try
         {
-            _ratingRepository = ratingRepository;
+            return Ok(await _ratingRepository.GetRatings());
         }
-
-        [HttpGet]
-        public async Task<ActionResult<IEnumerable<Rating>>> GetRatings()
+        catch (Exception ex)
         {
-            try
-            {
-                return Ok(await _ratingRepository.GetRatings());
-            }
-            catch (Exception ex)
-            {
-                return StatusCode(StatusCodes.Status500InternalServerError,
-                    ex.Message);
-            }
+            return StatusCode(StatusCodes.Status500InternalServerError,
+                ex.Message);
         }
+    }
 
-        [HttpGet("{id:int}")]
-        public async Task<ActionResult<Rating>> GetRating(int id)
-        {
-            if (id <= 0)
-                return BadRequest();
+    [HttpGet("{id:int}")]
+    public async Task<ActionResult<Rating>> GetRating(int id)
+    {
+        if (id <= 0)
+            return BadRequest();
             
-            try
-            {
-                var result = await _ratingRepository.GetRating(id);
-                if (result == null)
-                    return NotFound();
-
-                return Ok(result);
-            }
-            catch (Exception ex)
-            {
-                return StatusCode(
-                        StatusCodes.Status500InternalServerError,
-                        ex.Message);
-            }
-        }
-
-        [HttpPost]
-        public async Task<ActionResult<Rating>> CreateRating(Rating rating)
+        try
         {
-            try
-            {
-                var createdRating = await _ratingRepository.AddRating(rating);
+            var result = await _ratingRepository.GetRating(id);
+            if (result == null)
+                return NotFound();
+
+            return Ok(result);
+        }
+        catch (Exception ex)
+        {
+            return StatusCode(
+                StatusCodes.Status500InternalServerError,
+                ex.Message);
+        }
+    }
+
+    [HttpPost]
+    public async Task<ActionResult<Rating>> CreateRating(Rating rating)
+    {
+        try
+        {
+            var createdRating = await _ratingRepository.AddRating(rating);
                 
-                return Ok(CreatedAtAction(nameof(GetRating), 
-                    new { id = createdRating.Id }, createdRating));
-            }
-            catch (Exception ex)
-            {
-                return StatusCode(
-                        StatusCodes.Status500InternalServerError,
-                        ex.Message);
-            }
+            return Ok(createdRating.Id);
         }
-
-        [HttpPut]
-        public async Task<ActionResult<Rating>> UpdateRating(Rating rating)
+        catch (Exception ex)
         {
-            if (rating.Id <= 0)
+            return StatusCode(
+                StatusCodes.Status500InternalServerError,
+                ex.Message);
+        }
+    }
+
+    [HttpPut]
+    public async Task<ActionResult<Rating>> UpdateRating(Rating rating)
+    {
+        if (rating.Id <= 0)
+            return BadRequest();
+
+        try
+        {
+            var ratingToUpdate = await _ratingRepository.GetRating(rating.Id);
+            if (ratingToUpdate == null)
+                return NotFound();
+
+            var updatedRating = await _ratingRepository.UpdateRating(rating);
+            if (updatedRating == null)
                 return BadRequest();
 
-            try
-            {
-                var ratingToUpdate = await _ratingRepository.GetRating(rating.Id);
-                if (ratingToUpdate == null)
-                    return NotFound();
-
-                var updatedRating = await _ratingRepository.UpdateRating(rating);
-                if (updatedRating == null)
-                    return BadRequest();
-
-                return Ok(updatedRating.Id);
-            }
-            catch (Exception ex)
-            {
-                return StatusCode(
-                        StatusCodes.Status500InternalServerError,
-                        ex.Message);
-            }
+            return Ok(updatedRating.Id);
         }
-
-        [HttpDelete("{id:int}")]
-        public async Task<ActionResult<bool>> DeleteRating(int id)
+        catch (Exception ex)
         {
-            try
-            {
-                if (!await _ratingRepository.DeleteRating(id))
-                    return NotFound();
+            return StatusCode(
+                StatusCodes.Status500InternalServerError,
+                ex.Message);
+        }
+    }
 
-                return Ok();
-            }
-            catch (Exception ex)
-            {
-                return StatusCode(StatusCodes.Status500InternalServerError,
-                   ex.Message);
-            }
+    [HttpDelete("{id:int}")]
+    public async Task<ActionResult<bool>> DeleteRating(int id)
+    {
+        try
+        {
+            if (!await _ratingRepository.DeleteRating(id))
+                return NotFound();
+
+            return Ok();
+        }
+        catch (Exception ex)
+        {
+            return StatusCode(StatusCodes.Status500InternalServerError,
+                ex.Message);
         }
     }
 }
-
