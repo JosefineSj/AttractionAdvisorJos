afd6ddfd3a7688038eb54164794d6f61f93ba30f
Salam4nder <kamran.personal@icloud.com>
2023-02-02 23:48:11 +0100

    update

--- restAPI/AttractionAdvisor/AttractionAdvisor/Models/User.cs
+++ restAPI/AttractionAdvisor/AttractionAdvisor/Models/User.cs
@@ -5,6 +5,7 @@ namespace AttractionAdvisor.Models
 {
     public class User
     {
+        [JsonIgnore]
         public int Id { get; set; }
         public string Username { get; set; }
         public string Password { get; set; }    
